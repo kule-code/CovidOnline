616749f9c6069fa4d48c2ceb734a57edc171c381
Vihanga kulasekara <57080664+kulasekara02@users.noreply.github.com>
2023-11-10 16:23:37 +0530

    Update userLogout.php

--- userLogout.php
+++ userLogout.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
-unset($_SESSION["id"]);
-unset($_SESSION["name"]);
-header("Location:index.php");
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Clear the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 3600,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Redirect to the login page or any other page after logout
+header("Location: index.php");
+exit();
 ?>
